8207ddca8939d23b19d587dddd1352b88d4b1dfe
gustavoemf <gustavoeduardomf@gmail.com>
2023-09-01 10:48:34 -0400

    feat(AreaDTO): criação da DTO Area

---
+++ src/main/java/stag/ease/stagease/DTO/AreaDTO.java
@@ -0,0 +1,12 @@
+package stag.ease.stagease.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class AreaDTO extends AbstractDTO{
+    private String nomeArea;
+}
