09fcac5daec8e0aab2f44a572c40ebcdf2111dae
gustavoemf <gustavoeduardomf@gmail.com>
2023-09-01 10:49:44 -0400

    feat(AreaRepository): criação da repository de Area

---
+++ src/main/java/stag/ease/stagease/Repository/AreaRepository.java
@@ -0,0 +1,7 @@
+package stag.ease.stagease.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import stag.ease.stagease.Entity.AreaEntity;
+
+public interface AreaRepository extends JpaRepository<AreaEntity, Long> {
+}
