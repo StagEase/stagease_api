c4c08fcb0c536ba7940f09d7c152893994a72592
gustavoemf <gustavoeduardomf@gmail.com>
2023-09-02 18:04:48 -0400

    feat(UBSEntity): elementos da entidade e cardinalidade

--- src/main/java/stag/ease/stagease/Entity/UBSEntity.java
+++ src/main/java/stag/ease/stagease/Entity/UBSEntity.java
@@ -1,4 +1,47 @@
 package stag.ease.stagease.Entity;
 
-public class UBSEntity {
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import stag.ease.stagease.Entity.Enum.Distrito;
+
+import java.util.List;
+
+@Getter @Setter
+@Entity
+@Table(name = "ubs", schema = "stagease")
+public class UBSEntity extends AbstractEntity{
+    @Column(length = 50, nullable = false, unique = true)
+    private String nomeArea;
+
+    @Column(length = 50, nullable = false)
+    private String gerente;
+
+    @Enumerated(EnumType.STRING)
+    @Column
+    private Distrito distrito;
+
+    @Column
+    private List<String> contatos;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "ubs_supervisor",
+            joinColumns = @JoinColumn(name = "ubs_id"),
+            inverseJoinColumns = @JoinColumn(name = "supervisor_id"))
+    private List<SupervisorEntity> supervisor;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "ubs_area",
+            joinColumns = @JoinColumn(name = "ubs_id"),
+            inverseJoinColumns = @JoinColumn(name = "area_id"))
+    private List<AreaEntity> areasUBS;
+
+    @OneToMany(mappedBy = "ubs")
+    @JsonIgnore
+    private List<SolicitacaoEntity> solicitacoes;
+
+    @Column(length = 255)
+    private String descricao;
+
 }
