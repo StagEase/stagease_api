9926ec24386a0c52aae8511782b28d1c63c57cb1
gustavoemf <gustavoeduardomf@gmail.com>
2023-09-02 18:17:40 -0400

    feat(UBSRepository): busca por nome

--- src/main/java/stag/ease/stagease/Repository/UBSRepository.java
+++ src/main/java/stag/ease/stagease/Repository/UBSRepository.java
@@ -1,7 +1,9 @@
 package stag.ease.stagease.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import stag.ease.stagease.Entity.AreaEntity;
 import stag.ease.stagease.Entity.UBSEntity;
 
 public interface UBSRepository extends JpaRepository<UBSEntity, Long> {
+    UBSEntity findByNomeUBS(String nome);
 }
