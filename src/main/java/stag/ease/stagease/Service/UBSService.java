a697cce99638b46631fb14288231c38e179d26f4
gustavoemf <gustavoeduardomf@gmail.com>
2023-09-02 18:17:04 -0400

    feat(UBSService): funções service

--- src/main/java/stag/ease/stagease/Service/UBSService.java
+++ src/main/java/stag/ease/stagease/Service/UBSService.java
@@ -1,7 +1,35 @@
 package stag.ease.stagease.Service;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import stag.ease.stagease.DTO.AreaDTO;
+import stag.ease.stagease.Entity.UBSEntity;
+import stag.ease.stagease.Repository.UBSRepository;
 
 @Service
 public class UBSService {
+    @Autowired
+    private UBSRepository repository;
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @Transactional
+    public UBSEntity create(AreaDTO dto) {
+        UBSEntity entity = new UBSEntity();
+        modelMapper.map(dto, entity);
+        repository.save(entity);
+
+        return entity;
+    }
+
+    @Transactional
+    public UBSEntity update(Long id, AreaDTO dto) {
+        UBSEntity entity = repository.findById(id).orElseThrow(() -> new RuntimeException("Não foi possível encontrar o registro informado"));
+        modelMapper.map(dto, entity);
+        repository.save(entity);
+
+        return entity;
+    }
 }
