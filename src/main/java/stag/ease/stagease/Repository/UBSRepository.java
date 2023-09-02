a99e8b2e706be55022f3267d1065428fad745fea
gustavoemf <gustavoeduardomf@gmail.com>
2023-09-02 18:10:04 -0400

    feat(UBSRepository): criando repository

---
+++ src/main/java/stag/ease/stagease/Repository/UBSRepository.java
@@ -0,0 +1,7 @@
+package stag.ease.stagease.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import stag.ease.stagease.Entity.UBSEntity;
+
+public interface UBSRepository extends JpaRepository<UBSEntity, Long> {
+}
