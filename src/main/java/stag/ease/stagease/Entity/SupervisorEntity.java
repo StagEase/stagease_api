2e402cb4f8966a3611f45fb47b1b671d508a3f10
gustavoemf <gustavoeduardomf@gmail.com>
2023-09-03 11:55:41 -0400

    refactor(SupervisorEntity): adicionando cardinalidade que faltava com a ubs em supervisor

--- src/main/java/stag/ease/stagease/Entity/SupervisorEntity.java
+++ src/main/java/stag/ease/stagease/Entity/SupervisorEntity.java
@@ -1,6 +1,7 @@
 package stag.ease.stagease.Entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -17,4 +18,6 @@ import java.util.List;
 public class SupervisorEntity extends AbstractEntity{
     @OneToMany(mappedBy = "supervisor")
     private List<SolicitacaoEntity> solicitacaoList;
+    @ManyToMany(mappedBy = "supervisorList")
+    private List<UBSEntity> ubsList;
 }
