c9a49eac5837f773d1386d7b82f52e1ece27c474
marcelonv011 <marcelonv011@gmail.com>
2023-09-05 19:37:16 -0300

    refactor supervisor dto extends abstract dto

--- src/main/java/stag/ease/stagease/DTO/SupervisorDTO.java
+++ src/main/java/stag/ease/stagease/DTO/SupervisorDTO.java
@@ -3,13 +3,13 @@ package stag.ease.stagease.DTO;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class SupervisorDTO {
-    private Long id;
-    private boolean ativo;
+public class SupervisorDTO extends AbstractDTO{
     //    @NotBlank(message = "Este campo não pode ser nulo")
     private String nomeSupervisor;
     //    @NotBlank(message = "Este campo não pode ser nulo")
