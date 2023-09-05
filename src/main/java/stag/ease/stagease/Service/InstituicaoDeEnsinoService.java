8ffe1a1b16f3d3251e59ed16a2c08f1acd7956f4
marcelonv011 <marcelonv011@gmail.com>
2023-09-05 09:58:35 -0300

    feat instituicao de ensino service

---
+++ src/main/java/stag/ease/stagease/Service/InstituicaoDeEnsinoService.java
@@ -0,0 +1,38 @@
+package stag.ease.stagease.Service;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
+import stag.ease.stagease.DTO.InstituicaoDeEnsinoDTO;
+import stag.ease.stagease.DTO.SolicitacaoDTO;
+import stag.ease.stagease.Entity.InstituicaoDeEnsinoEntity;
+import stag.ease.stagease.Entity.SolicitacaoEntity;
+import stag.ease.stagease.Repository.InstituicaoDeEnsinoRepository;
+
+@Service
+public class InstituicaoDeEnsinoService {
+    @Autowired
+    private InstituicaoDeEnsinoRepository repository;
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @Transactional
+    public InstituicaoDeEnsinoDTO create(InstituicaoDeEnsinoDTO dto) {
+        if (dto.getId() != null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "O id deve ser gerado pelo banco");
+        }
+        InstituicaoDeEnsinoEntity entity = repository.save(modelMapper.map(dto, InstituicaoDeEnsinoEntity.class));
+        InstituicaoDeEnsinoDTO resultDTO = modelMapper.map(entity, InstituicaoDeEnsinoDTO.class);
+        return resultDTO;
+    }
+
+    @Transactional
+    public InstituicaoDeEnsinoDTO update(Long id, InstituicaoDeEnsinoDTO dto) {
+        InstituicaoDeEnsinoEntity entity = repository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Não foi possível encontrar o registro informado"));
+        modelMapper.map(dto, repository.save(entity));
+        return modelMapper.map(entity, InstituicaoDeEnsinoDTO.class);
+    }
+}
