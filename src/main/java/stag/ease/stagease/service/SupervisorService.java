8072c0b26e75ae518fc9025b697599ee4316650a
marcelonv011 <marcelonv011@gmail.com>
2023-10-02 12:38:04 -0300

    feat supervisor services test

--- src/main/java/stag/ease/stagease/service/SupervisorService.java
+++ src/main/java/stag/ease/stagease/service/SupervisorService.java
@@ -3,11 +3,15 @@ package stag.ease.stagease.service;
 import jakarta.persistence.EntityNotFoundException;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 import stag.ease.stagease.dto.AreaDTO;
+import stag.ease.stagease.dto.SolicitacaoDTO;
 import stag.ease.stagease.dto.SupervisorDTO;
 import stag.ease.stagease.entity.AreaEntity;
+import stag.ease.stagease.entity.SolicitacaoEntity;
 import stag.ease.stagease.entity.SupervisorEntity;
 import stag.ease.stagease.repository.SupervisorRepository;
 import stag.ease.stagease.repository.UBSRepository;
@@ -57,17 +61,17 @@ public class SupervisorService {
 
     @Transactional
     public SupervisorDTO create(SupervisorDTO dto) {
+        if (dto.getId() != null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "O id deve ser gerado pelo banco");
+        }
         return modelMapper.map(repository.save(modelMapper.map(dto, SupervisorEntity.class)), SupervisorDTO.class);
     }
 
     @Transactional
     public SupervisorDTO update(Long id, SupervisorDTO dto) {
-        SupervisorEntity existingEntity = repository.findById(id)
-                .orElseThrow(() -> new EntityNotFoundException("Área não encontrada com o ID: " + id));
-
-        modelMapper.map(dto, existingEntity);
-
-        return modelMapper.map(repository.save(existingEntity), SupervisorDTO.class);
+        SupervisorEntity entity = repository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Não foi possível encontrar o registro informado"));
+        modelMapper.map(dto, repository.save(entity));
+        return modelMapper.map(entity, SupervisorDTO.class);
     }
 
     @Transactional
