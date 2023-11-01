8b9d6edc8670798956f4f3ac7b11bbe27edc4091
marcelonv011 <marcelonv011@gmail.com>
2023-11-01 18:33:27 -0300

    refactor instituicao controller

--- src/main/java/stag/ease/stagease/controller/InstituicaoDeEnsinoController.java
+++ src/main/java/stag/ease/stagease/controller/InstituicaoDeEnsinoController.java
@@ -1,16 +1,17 @@
 package stag.ease.stagease.controller;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 import stag.ease.stagease.dto.InstituicaoDeEnsinoDTO;
 import stag.ease.stagease.entity.InstituicaoDeEnsinoEntity;
 import stag.ease.stagease.repository.InstituicaoDeEnsinoRepository;
 import stag.ease.stagease.service.InstituicaoDeEnsinoService;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -20,50 +21,42 @@ public class InstituicaoDeEnsinoController {
     private InstituicaoDeEnsinoService service;
     @Autowired
     private InstituicaoDeEnsinoRepository repository;
+    @Autowired
+    private ModelMapper modelMapper;
 
     @GetMapping("/{id}")
     public ResponseEntity<InstituicaoDeEnsinoDTO> getById(@PathVariable("id") Long id) {
-        return new ResponseEntity<>(service.getById(id), HttpStatus.OK);
+        return new ResponseEntity<>(modelMapper.map(service.getById(id), InstituicaoDeEnsinoDTO.class), HttpStatus.OK);
     }
 
     @GetMapping("/nome/{nome}")
     public ResponseEntity<InstituicaoDeEnsinoDTO> getByNome(@PathVariable("nome") String nome) {
-        return new ResponseEntity<>(service.findByNome(nome), HttpStatus.OK);
+        return new ResponseEntity<>(modelMapper.map(service.findByNome(nome), InstituicaoDeEnsinoDTO.class), HttpStatus.OK);
     }
 
     @GetMapping("/list")
     public ResponseEntity<List<InstituicaoDeEnsinoDTO>> getAll() {
-        return new ResponseEntity<>(service.getAll(), HttpStatus.OK);
+        List<InstituicaoDeEnsinoDTO> list = new ArrayList<>();
+        for (InstituicaoDeEnsinoEntity entity : service.getAll()) {
+            InstituicaoDeEnsinoDTO map = modelMapper.map(entity, InstituicaoDeEnsinoDTO.class);
+            list.add(map);
+        }
+        return new ResponseEntity<>(list, HttpStatus.OK);
     }
 
     @PostMapping
     public ResponseEntity<InstituicaoDeEnsinoDTO> create(@RequestBody @Validated InstituicaoDeEnsinoDTO dto) {
-        return new ResponseEntity<>(service.create(dto), HttpStatus.CREATED);
+        return new ResponseEntity<>(modelMapper.map(service.create(modelMapper.map(dto, InstituicaoDeEnsinoEntity.class)), InstituicaoDeEnsinoDTO.class), HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<InstituicaoDeEnsinoDTO> update(@PathVariable("id") Long id, @RequestBody @Validated InstituicaoDeEnsinoDTO dto) {
-        return new ResponseEntity<>(service.update(id, dto), HttpStatus.OK);
+        return new ResponseEntity<>(modelMapper.map(service.update(id, modelMapper.map(dto, InstituicaoDeEnsinoEntity.class)), InstituicaoDeEnsinoDTO.class), HttpStatus.OK);
     }
 
     @DeleteMapping("{id}")
-    public ResponseEntity<String> delete(@PathVariable("id") Long id) {
-        InstituicaoDeEnsinoEntity database = repository.findById(id).orElse(null);
-
-        if (database == null) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Registro não encontrado");
-        }
-
-        try {
-            repository.delete(database);
-            return ResponseEntity.ok("Registro deletado com sucesso!");
-        } catch (RuntimeException e) {
-            if (database.isAtivo()) {
-                database.setAtivo(false);
-                repository.save(database);
-                throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Erro no delete, flag desativada!");
-            }
-            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Erro no delete, a flag já está desativada");
-        }
+    public ResponseEntity<HttpStatus> delete(@PathVariable("id") Long id) {
+        service.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
--- src/main/java/stag/ease/stagease/service/InstituicaoDeEnsinoService.java
+++ src/main/java/stag/ease/stagease/service/InstituicaoDeEnsinoService.java
@@ -7,12 +7,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
-import stag.ease.stagease.dto.InstituicaoDeEnsinoDTO;
 import stag.ease.stagease.entity.InstituicaoDeEnsinoEntity;
-import stag.ease.stagease.entity.SupervisorEntity;
 import stag.ease.stagease.repository.InstituicaoDeEnsinoRepository;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
